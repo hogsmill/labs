c817a8680bed5135566f5c83d68ae3e750f1d804
hogsmill <info@hogsmill.com>
2021-09-03 14:41:57 +0100

    loon

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,38 @@
 import io from 'socket.io-client'
 import bus from './EventBus'
 
-let connStr
-if (location.hostname == 'localhost') {
+const prod = location.hostname != 'localhost'
+
+let asConnStr, connStr
+if (!prod) {
+  asConnStr = 'http://localhost:3099'
   connStr = 'http://localhost:3013'
 } else {
-  connStr = 'https://agilesimulations.co.uk:3013'
+  asConnStr = 'https://agilesimulations.co.uk:3099'
+  connStr = 'https://agilesimulations.co.uk:' + process.env.VUE_APP_PORT
 }
 console.log('Connecting to: ' + connStr)
 const socket = io(connStr)
+
+const connectToAgileSimulations = location.hostname != 'localhost'
+let asSocket
+if (connectToAgileSimulations) {
+  console.log('Connecting to: ' + asConnStr)
+  asSocket = io(asConnStr)
+}
+
+// Agile Simulations (login)
+
+if (connectToAgileSimulations) {
+
+  bus.$on('sendCheckLogin', (data) => { asSocket.emit('sendCheckLogin', data) })
+
+  bus.$on('sendRating', (data) => { asSocket.emit('sendRating', data) })
+
+  asSocket.on('loginSuccess', (data) => { bus.$emit('loginSuccess', data) })
+
+  asSocket.on('logout', (data) => { bus.$emit('logout', data) })
+}
 
 // Send
 
